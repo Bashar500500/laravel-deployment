0fd3525bd058f137c745d71932c06b5febda74a2
Bashar500500 <Aragon500500@gmail.com>
2025-05-30 03:17:46 +0300

    #28 try

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -8,6 +8,18 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Running seeders..."
+php artisan db:seed
+
+echo "Running passport..."
+php artisan passport:client --personal
+
+echo "Running passport..."
+php artisan passport:keys
+
 # echo "Publishing cloudinary provider..."
 # php artisan vendor:publish --provider="CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider" --tags="cloudinary-laravel-config"
 
