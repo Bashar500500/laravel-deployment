77b603afee7806d137ef94021f12b61f4cb4f573
Bashar500500 <Aragon500500@gmail.com>
2025-05-31 07:40:09 +0300

    #37 try

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://laravel-deployment-jl5s.onrender.com'),
+    'url' => env('APP_URL', 'https://laravel-deployment-fi4f.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
